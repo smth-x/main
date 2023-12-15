7d6e918a7d97f674ca826e74c5ba9da66eb66304
smth <readme.txt3@gmail.com>
2023-12-16 00:12:06 +0200

    added example of program

---
+++ src/SOLID/ExampleOfProgram.java
@@ -0,0 +1,31 @@
+package SOLID;
+
+public class ExampleOfProgram {
+    User user = new User("Alex","Surm","AlexSurm@gmail.com");
+    User.UserBankAccount firstBankAccount = new User.UserBankAccount(500,2000,"USD","regular",user);
+    User.UserBankAccount secondBankAccount = new User.UserBankAccount(1500,50250,"EUR","premium",user);
+    Transaction transaction = new Transaction();
+    ChangeCurrency changeCurrency = new ChangeCurrency();
+    public void example(){
+        //work with the first acc:
+        showBalanceOnFirstAcc();
+        changeCurrency.USDtoEUR(firstBankAccount);
+        showBalanceOnFirstAcc();
+        changeCurrency.EURtoUSD(firstBankAccount);
+        showBalanceOnFirstAcc();
+    }
+    public void showBalanceOnFirstAcc(){
+        System.out.println("");
+        System.out.println(firstBankAccount.cash);
+        System.out.println(firstBankAccount.balance);
+        System.out.println(firstBankAccount.currentCurrency);
+        System.out.println("");
+    }
+    public void showBalanceOnSecondAcc(){
+        System.out.println("");
+        System.out.println(secondBankAccount.cash);
+        System.out.println(secondBankAccount.balance);
+        System.out.println(secondBankAccount.currentCurrency);
+        System.out.println("");
+    }
+}
