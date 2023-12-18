50685141189432517c33335c4facacdcd6f8027b
smth <readme.txt3@gmail.com>
2023-12-18 19:57:58 +0200

    re writed logistic of Transaction.java class

--- src/SOLID/Transaction.java
+++ src/SOLID/Transaction.java
@@ -2,23 +2,15 @@ package SOLID;
 
 public class Transaction {
 
-    private UserType regularUser = new RegularUser();
-    private UserType premiumUser = new PremiumUser();
-
     public void withdraw(User.UserBankAccount bankAccount, int howMuchMoney) {
-        if (bankAccount.balance>=howMuchMoney) {
-            bankAccount.balance -= howMuchMoney;
-            switch (bankAccount.typeOfAccout) {
-                case "regular":
-                    System.out.println("success");
-                    bankAccount.cash += regularUser.withdrawMoneyWithTax(howMuchMoney);
-                    break;
-                case "premium":
-                    System.out.println("success");
-                    bankAccount.cash += premiumUser.withdrawMoneyWithTax(howMuchMoney);
-                    break;
-                default:
-                    System.out.println("unknown typeOfAccount");
+        if (bankAccount.balance >= howMuchMoney) {
+            UserType userType = getUserType(bankAccount);
+            if (userType != null) {
+                bankAccount.balance -= howMuchMoney;
+                bankAccount.cash += userType.withdrawMoneyWithTax(howMuchMoney);
+                System.out.println("success");
+            } else {
+                System.out.println("unknown typeOfAccount");
             }
         } else {
             System.out.println("not enough money on balance");
@@ -26,22 +18,28 @@ public class Transaction {
     }
 
     public void deposit(User.UserBankAccount bankAccount, int howMuchMoney) {
-        if (bankAccount.cash>=howMuchMoney) {
-            bankAccount.cash -= howMuchMoney;
-            switch (bankAccount.typeOfAccout) {
-                case "regular":
-                    System.out.println("success");
-                    bankAccount.balance += regularUser.depositMoneyWithTax(howMuchMoney);
-                    break;
-                case "premium":
-                    System.out.println("success");
-                    bankAccount.balance += premiumUser.depositMoneyWithTax(howMuchMoney);
-                    break;
-                default:
-                    System.out.println("unknown typeOfAccount");
+        if (bankAccount.cash >= howMuchMoney) {
+            UserType userType = getUserType(bankAccount);
+            if (userType != null) {
+                bankAccount.cash -= howMuchMoney;
+                bankAccount.balance += userType.depositMoneyWithTax(howMuchMoney);
+                System.out.println("success");
+            } else {
+                System.out.println("unknown typeOfAccount");
             }
         } else {
             System.out.println("not enough cash");
+        }
+    }
+
+    private UserType getUserType(User.UserBankAccount bankAccount) {
+        switch (bankAccount.typeOfAccout) {
+            case "regular":
+                return new RegularUser();
+            case "premium":
+                return new PremiumUser();
+            default:
+                return null;
         }
     }
 }
